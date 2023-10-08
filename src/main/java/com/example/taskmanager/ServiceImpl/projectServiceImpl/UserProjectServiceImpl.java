cdc3c3bdb9fa70a68e8780bee360264e5fd333d3
GIAKHOILEE <giakhoilee@gmail.com>
2023-10-08 21:41:34 +0700

    feat: CRUD UserProject, decentralization

--- src/main/java/com/example/taskmanager/ServiceImpl/projectServiceImpl/UserProjectServiceImpl.java
+++ src/main/java/com/example/taskmanager/ServiceImpl/projectServiceImpl/UserProjectServiceImpl.java
@@ -1,6 +1,67 @@
 package com.example.taskmanager.ServiceImpl.projectServiceImpl;
 
+import com.example.taskmanager.DTO.projectDTO.UserProjectDTO;
+import com.example.taskmanager.entity.UserEntity;
+import com.example.taskmanager.entity.projectEntity.ProjectEntity;
+import com.example.taskmanager.entity.projectEntity.UserProjectEntity;
+import com.example.taskmanager.repository.UserRepository;
+import com.example.taskmanager.repository.projectRepository.ProjectRepository;
+import com.example.taskmanager.repository.projectRepository.UserProjectRepository;
 import com.example.taskmanager.service.projectService.UserProjectService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
 public class UserProjectServiceImpl implements UserProjectService {
+    private final UserProjectRepository userProjectRepository;
+    private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
+
+    @Override
+    public UserProjectEntity addMemberToProject(UserProjectDTO userProjectDTO) {
+        UserEntity user = userRepository.findById(userProjectDTO.getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + userProjectDTO.getUserId()));
+
+        ProjectEntity project = projectRepository.findById(userProjectDTO.getProjectId())
+                .orElseThrow(() -> new RuntimeException("Project not found with id: " + userProjectDTO.getProjectId()));
+
+        UserProjectEntity userProject = new UserProjectEntity();
+        userProject.setUser(user);
+        userProject.setProject(project);
+        userProject.setRole(userProjectDTO.getRole());
+
+        return userProjectRepository.save(userProject);
+    }
+
+    @Override
+    public void removeMemberFromProject(UserProjectDTO userProjectDTO) {
+        UserEntity user = userRepository.findById(userProjectDTO.getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + userProjectDTO.getUserId()));
+
+        ProjectEntity project = projectRepository.findById(userProjectDTO.getProjectId())
+                .orElseThrow(() -> new RuntimeException("Project not found with id: " + userProjectDTO.getProjectId()));
+
+        UserProjectEntity userProject = userProjectRepository.findByUserAndProject(user, project)
+                .orElseThrow(() -> new RuntimeException("User not associated with this project"));
+
+        userProjectRepository.delete(userProject);
+    }
+
+    @Override
+    public List<UserEntity> findUsersByProjectId(Long projectId){
+        List<UserProjectEntity> userProjects = userProjectRepository.findByProject_ProjectId(projectId);
+
+        return userProjects.stream()
+                .map(UserProjectEntity::getUser)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<UserProjectEntity> findUserProjectsByProjectId(Long projectId){
+        return userProjectRepository.findAllByProject_ProjectId(projectId);
+    }
 }
--- src/main/java/com/example/taskmanager/repository/UserRepository.java
+++ src/main/java/com/example/taskmanager/repository/UserRepository.java
@@ -2,9 +2,14 @@ package com.example.taskmanager.repository;
 
 import com.example.taskmanager.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
+    List<UserEntity> findByUserIdNotIn(List<Long> userIds);
+
 }
